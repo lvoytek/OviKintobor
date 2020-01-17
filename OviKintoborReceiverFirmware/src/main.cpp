bdc9443c6562834f0a78513ed26a9f4a48763c03
Lena Voytek <dvoytek@email.arizona.edu>
2020-01-17 01:20:47 -0700

    Main brief

--- OviKintoborReceiverFirmware/src/main.cpp
+++ OviKintoborReceiverFirmware/src/main.cpp
@@ -1,9 +1,23 @@
+/*  
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. 
+ * 
+ * Author: Lena Voytek
+ * 
+ * main.cpp
+ * Handles all sensor interaction and communication on the receiver 
+ * module
+ */
+
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+
 }
