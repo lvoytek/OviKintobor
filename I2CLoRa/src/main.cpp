d857b79a92896fe2686fa54a6baae47b0b0b5b6b
Lena Voytek <dvoytek@email.arizona.edu>
2020-01-17 07:47:47 -0700

    Finished maybe

--- I2CLoRa/src/main.cpp
+++ I2CLoRa/src/main.cpp
@@ -1,27 +1,72 @@
 #include <Arduino.h>
+#include <Wire.h>
 #include <LoRaCommunicator.h>
 
 LoRaCommunicator radio;
-char data[255];
+char incomingBuffer[255][10];
+char outgoing[255];
 
+int outgoingLoc = 0;
+
+int lastMessageLoc = 0;
+int firstMessageLoc = 0;
+
+void i2cRecv(int number)
+{
+	while(Wire.available() && outgoingLoc < 254)
+	{
+		outgoing[outgoingLoc] = Wire.read();
+		if(outgoing[outgoingLoc] == '\0')
+		{
+			radio.sendString(outgoing);
+			outgoingLoc = 0;
+		}
+	}
+
+	if(outgoingLoc == 254)
+	{
+		outgoing[outgoingLoc] = '\0';
+		radio.sendString(outgoing);
+		outgoingLoc = 0;
+	}
+}
+
+void messageRequest()
+{
+	if(lastMessageLoc == firstMessageLoc)
+		Wire.write('\0');
+	
+	else
+	{
+		char * tempChr = incomingBuffer[firstMessageLoc ++];
+
+		while(*tempChr != '\0')
+			Wire.write(*tempChr);
+
+		Wire.write('\0');
+
+		firstMessageLoc = firstMessageLoc%10;
+	}
+	
+}
 
 void setup()
 {
+	Wire.begin(5);
+	Wire.onReceive(i2cRecv);
+	Wire.onRequest(messageRequest);
 	radio.begin();
-	Serial.begin(9600);
+	
 }
 
 void loop()
 {
-	if(Serial.available())
+
+	if(radio.receiveString(incomingBuffer[lastMessageLoc]))
 	{
-		radio.sendString(Serial.readString().c_str());
+		lastMessageLoc++;
+		lastMessageLoc = lastMessageLoc%10;
 	}
 
-	if(radio.receiveString(data))
-	{
-		Serial.println(data);
-	}
-
-	delay(50);
+	delay(25);
 }
