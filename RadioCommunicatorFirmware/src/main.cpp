9cc2867745fd63fb737df03fba9cc2ae702b686c
Lena Voytek <dvoytek@email.arizona.edu>
2020-01-17 07:00:56 -0700

    Finished simple comm app

--- RadioCommunicatorFirmware/src/main.cpp
+++ RadioCommunicatorFirmware/src/main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
+#include <LoRaCommunicator.h>
 
-void setup() {
-  // put your setup code here, to run once:
+LoRaCommunicator radio;
+char data[255];
+
+
+void setup()
+{
+	radio.begin();
+	Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+	if(Serial.available())
+	{
+		radio.sendString(Serial.readString().c_str());
+	}
+
+	if(radio.receiveString(data))
+	{
+		Serial.println(data);
+	}
+
+	delay(50);
 }
