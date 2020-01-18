2ed864d6e6e1569196b3441d80b7748b9649c85f
Lucas Boje <boje@email.arizona.edu>
2020-01-18 09:46:46 -0800

    lets start parsing data, even if we can't receive or send it yet

---
+++ Knuckles/DataParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knuckles
+{
+	class DataParser
+	{
+		String RadioParser(String input)
+		{
+			char CommandType = input.ElementAt(0);
+			
+			//already in the format we want it
+			if(CommandType.Equals('G') || CommandType.Equals('B'))
+			{
+				return input;
+			}
+			return "";
+		}
+	}
+}
