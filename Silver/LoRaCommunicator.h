29ea42a1caedee424095678d3a44df5d63591d01
Lucas Boje <boje@email.arizona.edu>
2020-01-18 07:12:00 -0800

    we might be able to communicate someday

---
+++ Silver/LoRaCommunicator.h
@@ -0,0 +1,84 @@
+/*  
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. 
+ * 
+ * Author: Lena Voytek
+ * 
+ * LoRaCommunicator.h
+ * Handles sending and receiving of radio info over 915MHz LoRa radio
+ */
+
+#ifndef LORACOMMUNICATOR_H
+#define LORACOMMUNICATOR_H
+
+#define RADIO_CSPIN 8 //The Circuit Select Pin used to toggle SPI Comms On/Off
+#define RFM95_CS 8 //Radio chipset pin
+#define RFM95_RST 4 //Radio reset pin
+#define RFM95_INT 3 //Radio SPI pin
+
+#include <RH_RF95.h>
+
+
+class LoRaCommunicator
+{
+protected:
+    RH_RF95 rf95 = RH_RF95(RFM95_CS, RFM95_INT);
+
+    float frequency = 915.5;
+
+    void CSEnable(boolean enable)
+	{
+		if(enable)
+			digitalWrite(RADIO_CSPIN, LOW);	//Drop LOW to enable SPI Comms
+		else
+			digitalWrite(RADIO_CSPIN, HIGH); //Set HIGH to disable SPI Comms
+	}
+
+public:
+    void begin()
+    {
+        pinMode(RADIO_CSPIN, OUTPUT);
+        this->CSEnable(true);
+        pinMode(LED_BUILTIN, OUTPUT);
+
+        pinMode(RFM95_RST, OUTPUT);
+		
+		// Reset the Radio
+		digitalWrite(RFM95_RST, HIGH);
+		delay(1000);
+		digitalWrite(RFM95_RST, LOW);
+		delay(10);
+		digitalWrite(RFM95_RST, HIGH);
+		delay(10);
+
+        this->rf95.init();
+        this->rf95.setFrequency(this->frequency);
+        this->rf95.setTxPower(23, false);
+        this->CSEnable(false);
+        digitalWrite(LED_BUILTIN, LOW);
+    }
+
+    void sendString(const char * str)
+    {
+        digitalWrite(LED_BUILTIN, HIGH);
+        this->CSEnable(true);
+        this->rf95.send((byte *) str, strlen(str) + 1);
+        this->rf95.waitPacketSent();
+        this->CSEnable(false);
+        digitalWrite(LED_BUILTIN, LOW);
+    }
+
+    bool receiveString(char * output)
+    {
+        this->CSEnable(true);
+        uint8_t len;
+        bool received = this->rf95.recv((byte *) output, &len);
+
+        this->CSEnable(false);
+        return received;
+    }
+
+};
+
+#endif
--- Silver/Silver.ino
+++ Silver/Silver.ino
@@ -1,46 +1,27 @@
+#include "LoRaCommunicator.h"
 
+
+LoRaCommunicator radio;
+char data[255];
 
 void setup() {
+  radio.begin();
   Serial.begin(9600);
-
 }
 
-//Reads any inputs that may come in over serial
-void readFromSerial(){
-  char inputStr[255];
-  int index = 0;
-  bool done;
-  
-  while (true){
-    done = false;
+void loop()
+{
 
-    while(!done){
-      while(Serial.available() > 0){ //Incoming communications
-        char inputChar = Serial.read();
-        if (inputChar == '\n') { //It's not over til the newline
-          done = true;                
-          if(index < 255){
-            inputStr[index] = '\0';
-          }
-        } 
-        else {
-          if (index < 255) { //We don't want to exceed the array size
-            inputStr[index] = inputChar;                  
-            index++;
-          }
-        }
-      }     
-    }
-
-    //Ensure the incoming message is properly formatted. If so, move the arm.
-    if (inputStr[0] == 'I') {  
-      Serial.println(inputStr);
-      return;
-    }
+  if(Serial.available() > 0)
+  {
+    radio.sendString(Serial.readString().c_str());
   }
-}
+ 
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  readFromSerial();
+  if(radio.receiveString(data))
+  {
+    Serial.println(data);
+  }
+
+  delay(50);
 }
